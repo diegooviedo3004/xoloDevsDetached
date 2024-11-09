ab0c224d31199fc3f832d4caef524e29ef6588cb
xSarscov <vargasernest7@gmail.com>
2024-11-08 23:24:50 -0600

    chore

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -99,7 +99,7 @@ class Post(models.Model):
     draft = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.descripcion[:20]} - {self.user.username}"
+        return f"{self.descripcion[:20]}"
 
 class PostImage(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
